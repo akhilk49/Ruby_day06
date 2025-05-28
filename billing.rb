2663a45cb8d72f0e033055ce0ca920b6ef58be4d
akhilk49 <akhilkkurian49@gmail.com>
2025-05-28 15:51:27 +0530

    updated code using rescue blocks and debugging with pry

--- billing.rb
+++ billing.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def generate_bill(item:, price:, quantity: 1, discount: 0)
   total = price * quantity
   discounted_total = total - (total * discount / 100.0)
@@ -10,21 +12,32 @@ def generate_bill(item:, price:, quantity: 1, discount: 0)
   puts "Total after discount: #{discounted_total.round(2)}"
 end
 
-puts "Enter the number of items:"
-n = gets.to_i  
+begin
+  puts "Enter the number of items:"
+  n = gets.to_i
 
-n.times do |i|  
-  puts "\nEnter the item name:"
-  item = gets.chomp
+  n.times do |i|
+    puts "\nEnter the item name:"
+    item = gets.chomp
 
-  puts "Enter the price per unit:"
-  price = gets.to_f  
+    puts "Enter the price per unit:"
+    price = Float(gets) rescue nil
+    raise "Invalid price entered" if price.nil? || price < 0
 
-  puts "Enter the item quantity:"
-  quantity = gets.to_i  
+    puts "Enter the item quantity:"
+    quantity = Integer(gets) rescue nil
+    raise "Invalid quantity entered" if quantity.nil? || quantity < 0
 
-  puts "Enter the discount % if applicable:"
-  discount = gets.to_f  
+    puts "Enter the discount % if applicable:"
+    discount = Float(gets) rescue 0
+    discount = 0 if discount < 0
 
-  generate_bill(item: item, price: price, quantity: quantity, discount: discount)
+    generate_bill(item: item, price: price, quantity: quantity, discount: discount)
+  end
+
+rescue => e
+  puts "Error: #{e.message}"
 end
+
+# To debug, uncomment the next line:
+# binding.pry
